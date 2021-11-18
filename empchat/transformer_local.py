76d4d9004ed5d85cdd820f44b3a0c49a78cae52f
Nizam Mohammed <nizish@gmail.com>
2021-11-18 15:00:05 +0000

    Fix RuntimeError
    a view of a leaf Variable that requires grad is being used in an in-place operation.

--- empchat/transformer_local.py
+++ empchat/transformer_local.py
@@ -22,8 +22,9 @@ def create_position_codes(n_pos, dim, out):
             for pos in range(n_pos)
         ]
     )
-    out[:, 0::2] = torch.FloatTensor(np.sin(position_enc[:, 0::2]))
-    out[:, 1::2] = torch.FloatTensor(np.cos(position_enc[:, 1::2]))
+    with torch.no_grad():
+        out[:, 0::2] = torch.FloatTensor(np.sin(position_enc[:, 0::2]))
+        out[:, 1::2] = torch.FloatTensor(np.cos(position_enc[:, 1::2]))
     out.detach_()
     out.requires_grad = False
 
